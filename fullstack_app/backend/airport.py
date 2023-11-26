8376ef37fa300e5ecfe4e520e91d8c2d581dae15
Kevin <kevin-nguyen129@hotmail.com>
2023-11-26 19:37:53 +0000

    Taken user response from post request to flask app

--- fullstack_app/backend/airport.py
+++ fullstack_app/backend/airport.py
@@ -1,6 +1,8 @@
 import json
 import os
+import airport_api
 from typing import NoReturn
+
 
 import requests
 from dotenv import load_dotenv
@@ -9,3 +11,9 @@ load_dotenv()
 
 airport_api_key = os.environ["airport_api_key"]
 weather_api_key = os.environ["weather_api_key"]
+
+user_response_json = airport_api.process_input()
+
+user_response = json.loads(user_response_json)
+
+print(user_response)
