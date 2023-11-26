9021095ee6018b532249437c4a0fcdf4b703cb04
Kevin <kevin-nguyen129@hotmail.com>
2023-11-26 20:14:01 +0000

    Type hinted process input, added function to load airports json file to python dict

--- fullstack_app/backend/airport.py
+++ fullstack_app/backend/airport.py
@@ -14,7 +14,13 @@ airport_api_key = os.environ["airport_api_key"]
 weather_api_key = os.environ["weather_api_key"]
 
 
-def process_input():
+def process_input() -> json:
+    """Takes response from flask app and return json object with user search query, and message
+
+    Returns:
+        json: users input, message
+    """
+
     try:
         user_input = request.json['user_input']
         print(user_input)
@@ -23,3 +29,27 @@ def process_input():
     except KeyError:
 
         return jsonify({"error": "Invalid request. 'user_input' key is missing."}), 400
+
+
+def load_airport_data() -> dict:
+    """Opens airport.json file and loads it, to access airport data. Data structure: {
+      "name": "",
+      "city": "",
+      "country": "",
+      "IATA": "",
+      "icao": "",
+      "lat": "",
+      "lon": "",
+      "timezone": ""
+    }
+
+      Returns:
+          list: List of dictionaries containing airports and information
+    """
+    with open("airports2.json", encoding="utf-8") as airport_file:
+        airport_list = json.load(airport_file)
+    return airport_list
+
+
+if __name__ == "__main__":
+    search_term = process_input()
